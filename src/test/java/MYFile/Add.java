38fb86ee830b0e23783b474aadfb283c510918ef
dada <dadasahebkale.dk@gmail.com>
2022-12-29 15:08:05 +0530

    this commit from add class

---
+++ src/test/java/MYFile/Add.java
@@ -0,0 +1,14 @@
+package MYFile;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=10;
+		b=4;
+		c=a+b;
+		System.out.println(c);
+	}
+
+}
